87f28c7c4b89f5c29212c4a6c41a698bbc9f1c8f
Berman21 <yosefberman21@gmail.com>
2023-07-03 14:27:52 +0300

    day 6 changes

--- Ball Project/Yosef Berman - intro 1.7.23 - day 5/js/day 5 main.js
+++ Ball Project/Yosef Berman - intro 1.7.23 - day 5/js/day 5 main.js
@@ -18,37 +18,34 @@ function changeBallText(ballText) {  // inserts user input text to ballText
 
 //////////////////////////////////       TASK 3      ///////////////////////////////////////////////
 
-changeElementColor(document.querySelector('h1'), 'lightpink') //call function with selected element and the color to apply
+changeElementColor('.ball', 'lightpink') //call function with selected element and the color to apply
 
 function changeElementColor(selector, color) { //insert selectged element to selector and light-pink to color
-    selector.style.backgroundColor = color  //change selected element bgc to selected color
+    document.querySelector(selector).style.backgroundColor = color  //change selected element bgc to selected color
     console.log('task 3 - the element background color changed');
 }
 
 ///////////////////////////////////      TASK 4       ////////////////////////////////////////////////
 
-var res = getMult(4, 20)                          // declare var res that calls function getMult with two numbers
+var res = getMult(4, 20)                        // declare var res that calls function getMult with two numbers
 console.log('task 4 - the returned mult result is: ' + res);    //print result to console
 
 function getMult(num1, num2) {                     // insert to num1 and num2 the numbers
-    var sum = num1 * num2                        //declare sum to hold the mult of the two numbers
-    return sum                                   // return sum to the var res that called the function getMult
+    var mult = num1 * num2                        //declare mult to hold the multiplaction of the two numbers
+    return mult                                   // return mult to the var res that called the function getMult
 }
 
 ////////////////////////////////////      TASK 5 - HIDE LINES 11-12 WHEN CHECKING     //////////////////////////////////////////////
 
-// var msg = getUserMsg(prompt('score?')) // get score from user and call function
-// changeBallText(msg) // call task 2 function using var msg instead of var text and print msg on ball
+var msg = getUserMsg(prompt('score?')) // get score from user and call function
+changeBallText(msg) // call task 2 function using var msg instead of var text and print msg on ball
 
-// function getUserMsg(score) {
-//     var msg1 = 'excellent'
-//     var msg2 = 'very good'
-//     var msg3 = 'maybe next time..'
-//     if (score >= 100) {
-//         return msg1               // return msg1 to msg if score >= 100
-//     } else if (score >= 80) {
-//         return msg2               // return msg2 to msg if score less than 100 but more than 80
-//     } else {
-//         return msg3               // return msg3 to msg if less than 80
-//     }
-// }
+function getUserMsg(score) {
+    if (score >= 100) {
+        return 'excellent!'               // return msg1 to msg if score >= 100
+    } else if (score >= 80) {
+        return 'very good!'                // return msg2 to msg if score less than 100 but more than 80
+    } else {
+        return 'maybe next time..'               // return msg3 to msg if less than 80
+    }
+}
---
+++ Ball Project/Yosef Berman - intro 1.7.23 - day 5/js/day 6 main.js
@@ -0,0 +1,62 @@
+// Task 1
+
+// var mult = 1
+// for (var i = 0; i < 10; i++){
+//     var num = prompt('enter a number')
+//     console.log('num:', num);
+//     mult = mult * num
+// }
+// console.log('the mult result of the numbers is: ',mult);
+
+// task 2
+
+// var names = ['Muki','Puki','Bobi','Charlie']
+// // console.log('array "names" length is: ',names.length)
+// // console.log(names[1],names[3])
+// for(var i = 0; i < names.length; i++){
+//     var currName = names[i]
+//     if(currName == 'Puki'){
+//         console.log('Hey ' + currName + '!, ' + 'i know you!')
+//     }else{
+//         console.log(currName);
+//     }
+// }
+
+//task 3
+
+// var numbers = [30,6,22,8,13,90]
+// for(var i = 0; i < numbers.length; i++){
+//     console.log('input numbers: ',numbers[i])
+//     if(numbers[i] > 10){
+//         console.log(numbers[i]);
+//     }else{
+//         console.log('(multiplied)',numbers[i]*10);
+//     }
+// }
+
+// task 4
+
+// var nameArray = getArrayFromUser(4)
+// console.log('nameArray:', nameArray);
+
+// function getArrayFromUser(length) {
+//     var names = []
+//     for (var i = 0; i < length; i++) {
+//         names[i] = prompt('name?')
+//         console.log('i:', i, names[i])
+//     }
+//     return names
+// }
+
+// task 5
+
+var arraySum = getSum([1,2,3,6,7])
+console.log('the array sum is:',arraySum);
+
+function getSum(nums){
+    var sum = 0
+    for(var i = 0; i < nums.length; i++){
+        sum = sum + nums[i]
+    }
+    return sum
+}
